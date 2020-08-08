1f2253644cc0199147be185a4349bf7db80443a5
gigakuma <qiang.l.x@gmail.com>
2020-08-08 13:55:28 +0800

    example: send image

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,16 @@
 import Bot from '../src/index';
 const bot = new Bot();
-bot.start('');
-bot.on('DISPATCH', (data) => {
-  console.log(data);
+bot.startWithPassword('tester#****', '***');
+bot.on('MESSAGE_CREATE', async (data) => {
+  const channelId = data.d.channel_id;
+  if (data.d.content === '/image') {
+    try {
+      await bot.api
+        .route(`/channels/${channelId}/messages`)
+        .post({ data: { content: 'abcd' }, files: ['./example/IMG_0348.png'] });
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
+  }
 });
